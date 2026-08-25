2937107c3ad5cbb9d49ce5c7f748066c2b66bbe3
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-25 20:01:19 +0400

    [AEGISORA-69133610] Implemented guardian.

--- src/RegexRuleGuardian.php
+++ src/RegexRuleGuardian.php
@@ -2,7 +2,11 @@
 
 namespace Aegisora\RuleGuardians\RegexRule;
 
+use Aegisora\Guardian\Exceptions\GuardianExecutingRuleException;
+use Aegisora\Guardian\Exceptions\GuardianValidationException;
 use Aegisora\Guardian\Guardian;
+use Aegisora\Rules\RegexRule;
+use Throwable;
 
 class RegexRuleGuardian
 {
@@ -12,5 +16,19 @@ class RegexRuleGuardian
         Guardian $guardian
     ) {
         $this->guardian = $guardian;
+    }
+
+    /**
+     * @param mixed $value
+     * @throws GuardianExecutingRuleException
+     * @throws GuardianValidationException
+     * @throws Throwable
+     */
+    public function check(
+        $value,
+        string $pattern,
+        ?Throwable $exception = null
+    ): void {
+        $this->guardian->check($value, RegexRule::create($pattern), $exception);
     }
 }
